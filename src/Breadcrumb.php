93973b217a4a375ecad49ee4a6eb6a980ebe7fb6
Adam Cooper <adam@networkpie.co.uk>
2015-03-21 17:06:08 +0000

    Separate logic out into business class. Rudimentary twig template.

---
+++ src/Breadcrumb.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Bolt\Extension\Cooperaj\Breadcrumb;
+
+use Bolt\Application;
+use Bolt\Menu;
+
+class Breadcrumb extends \ArrayObject
+{
+    private $route;
+    private $route_params;
+
+    public function __construct(Application $app, Menu $menu)
+    {
+        parent::__construct(array());
+
+        $this->route = $app['request']->get('_route');
+        $this->route_params = $app['request']->get('_route_params');
+
+        $breadcrumb = $this->initialise($menu);
+        $this->exchangeArray($breadcrumb);
+    }
+
+    protected function initialise(Menu $menu)
+    {
+        $reflection = new \ReflectionClass(get_class($this));
+        $method = 'process' . ucfirst($this->route);
+
+        if ($reflection->hasMethod($method)) {
+            return call_user_func(array($this, $method), $menu);
+        }
+    }
+
+    protected function pruneTree(array $menu, $path)
+    {
+        foreach ($menu as $menu_item)
+        {
+            if ($menu_item['path'] === $path) {
+                $menu_item['current'] = 'true';
+                return array($menu_item);
+            }
+
+            if (isset($menu_item['submenu'])) {
+                $sub_items = $this->pruneTree($menu_item['submenu'], $path);
+
+                if (!empty($sub_items)) {
+                    $menu_item['current'] = 'true';
+                    array_unshift($sub_items, $menu_item);
+                    return $sub_items;
+                }
+            }
+        }
+
+        return array();
+    }
+
+    protected function createPath() {
+        $path = '';
+
+        $path .= $this->route_params['contenttypeslug'] ?: '';
+        $path .= strlen($path) > 0 ? '/' : '';
+        $path .= $this->route_params['slug'] ?: '';
+
+        $path = trim($path, '/');
+
+        return $path;
+    }
+
+    protected function processPagebinding(Menu $menu)
+    {
+        $breadcrumb = $this->pruneTree($menu->getItems(), $this->createPath());
+
+        return $breadcrumb;
+    }
+
+    protected function processContentlisting(Menu $menu)
+    {
+        $breadcrumb = $this->pruneTree($menu->getItems(), $this->createPath());
+
+        return $breadcrumb;
+    }
+
+    protected function processTaxonomylink(Menu $menu)
+    {
+        $breadcrumb = $this->pruneTree($menu->getItems(), $this->createPath());
+
+        return $breadcrumb;
+    }
+
+    protected function processContentlink(Menu $menu)
+    {
+        $breadcrumb = $this->pruneTree($menu->getItems(), $this->createPath());
+
+        return $breadcrumb;
+    }
+
+}
--- src/Twig/BreadcrumbExtension.php
+++ src/Twig/BreadcrumbExtension.php
@@ -3,6 +3,7 @@
 namespace Bolt\Extension\Cooperaj\Breadcrumb\Twig;
 
 use Bolt\Application;
+use Bolt\Extension\Cooperaj\Breadcrumb\Breadcrumb;
 use Bolt\Extension\Cooperaj\Breadcrumb\Extension;
 
 class BreadcrumbExtension extends \Twig_Extension
@@ -57,7 +58,20 @@ class BreadcrumbExtension extends \Twig_Extension
         );
     }
 
-    public function twigBreadcrumb($menu, $template = '_sub_breadcrumb.twig') {
+    /**
+     * @param string $identifier The name of a bolt menu to render
+     * @param string $template The name of a template to use to render the menu
+     * @return \Twig_Markup
+     */
+    public function twigBreadcrumb($identifier, $template = '_sub_breadcrumb.twig')
+    {
+        /** @var \Bolt\Menu $resolvedMenu */
+        $resolvedMenu = $this->app['menu']->menu($identifier);
 
+        $breadcrumb = new Breadcrumb($this->app, $resolvedMenu);
+
+        $html = $this->app['render']->render($template, array('breadcrumb' => $breadcrumb));
+
+        return new \Twig_Markup($html, 'UTF-8');
     }
 }
